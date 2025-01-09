3e6386437f86e9ee9dcfd024208254410bf56308
Piotr Siedlak <piotr.siedlak@microsoft.wsei.edu.pl>
2025-01-09 22:39:43 +0100

    add 3 models

--- WebAppSylwek/Models/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebAppSylwek.Models
-{
-    public class Item
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-
-}
---
+++ WebAppSylwek/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppSylwek.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Proszę wprowadzić nazwę użytkownika")]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Display(Name = "Data urodzenia")]
+        public DateTime? Birthdate { get; set; }
+    }
+}
