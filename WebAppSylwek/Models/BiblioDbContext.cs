dffb56b721f15a5404915dec694729f83e43b507
Sylwester Szwedzki <sylwester.szwedzki@microsoft.wsei.edu.pl>
2025-01-09 21:19:08 +0100

    added db context

---
+++ WebAppSylwek/Models/BiblioDbContext.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+namespace WebAppSylwek.Models
+{
+    public class BiblioDbContext : DbContext
+    {       public BiblioDbContext(DbContextOptions<BiblioDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Item> Items { get; set; }
+    }
+}
--- WebAppSylwek/Program.cs
+++ WebAppSylwek/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+builder.Services.AddDbContext<WebAppSylwek.Models.BiblioDbContext>(options=> options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
